26d86c38643c0f8cceb49c89afe92d89ed235b94
leanor13 <leanor@yandex.ru>
2021-08-21 14:32:10 +0300

    added link to homepage

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,6 @@ from django.urls.resolvers import URLPattern
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('about', views.about, name = 'about')
 ]
