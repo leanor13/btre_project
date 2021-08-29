48516815f265072c71cd723722b0ae54550be3b6
leanor13 <leanor@yandex.ru>
2021-08-29 13:07:24 +0300

    adding initial listings and realtors

---
+++ listings/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+import listings
+
+urlpatterns = [
+    path('', views.index, name = 'listings'),
+    path('<int:listing_id>', views.listing, name = 'listing'),
+    path('search', views.search, name = 'search')
+
+]
