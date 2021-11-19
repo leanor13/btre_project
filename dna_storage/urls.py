37f8bac861779624f112e4e9d714bde8b74b806f
leanor13 <leanor@yandex.ru>
2021-11-19 13:14:13 +0200

    commented out not needed api links

--- dna_storage/urls.py
+++ dna_storage/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 import storage.views
-import storage.api_views
+# import storage.api_views
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
     path('', include('storage.urls')),
-    path('v2/api/', storage.api_views.SequenceList.as_view()),
-    path('v2/api/new', storage.api_views.SequenceCreate.as_view())
+#    path('v2/api/', storage.api_views.SequenceList.as_view()),
+#    path('v2/api/new', storage.api_views.SequenceCreate.as_view())
 ]
